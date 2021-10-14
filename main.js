ce7e7a0321d63bab9182823261d9d7390eb90100
Ricky Mormor <rickymormor1@gmail.com>
2021-10-14 17:19:23 +0000

    Format all files

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-const hamburger = document.getElementById('hamburger-icon');
-const open = document.querySelector('.open');
-const speakerList = document.querySelector('.speakers-lineout')
+const hamburger = document.getElementById("hamburger-icon");
+const open = document.querySelector(".open");
+const speakerList = document.querySelector(".speakers-lineout");
 
 const speakers = [
   6,
-  speaker = {
-    name: 'Yochi Mormor',
-    image: 'images/speaker.png',
-    summary: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-    summary2: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. At, quas?',
-  },
+  (speaker = {
+    name: "Yochi Mormor",
+    image: "images/speaker.png",
+    summary: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    summary2:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. At, quas?",
+  }),
 ];
 
 for (let i = 1; i <= speakers[0]; i += 1) {
@@ -23,17 +24,11 @@ for (let i = 1; i <= speakers[0]; i += 1) {
 </div>
 </div>`;
 
-speakerList.innerHTML += speakerCards;
+  speakerList.innerHTML += speakerCards;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-
-  
-
-  open.addEventListener('click', () => {
-    open.classList.toggle('oppenned')
-  })
-
+  open.addEventListener("click", () => {
+    open.classList.toggle("oppenned");
+  });
 });
-
-
