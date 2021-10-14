f4fb278322988eabd0a932c2062291880a112e85
Ricky Mormor <rickymormor1@gmail.com>
2021-10-14 17:12:37 +0000

    Generate speakers sextion with javascript and make page more responsive

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
 const hamburger = document.getElementById('hamburger-icon');
 const open = document.querySelector('.open');
+const speakerList = document.querySelector('.speakers-lineout')
 
+const speakers = [
+  6,
+  speaker = {
+    name: 'Yochi Mormor',
+    image: 'images/speaker.png',
+    summary: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    summary2: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. At, quas?',
+  },
+];
+
+for (let i = 1; i <= speakers[0]; i += 1) {
+  const speakerCards = `<div id="speaker-${i}" class="only-desktop">
+<img src="${speakers[1].image}" alt="Image of speaker">
+<div>
+<h3>${speakers[1].name}</h3>
+<p class="speaker-summary">${speakers[1].summary}</p>
+<hr>
+<p>${speakers[1].summary2}</p>
+</div>
+</div>`;
+
+speakerList.innerHTML += speakerCards;
+}
 
 document.addEventListener("DOMContentLoaded", () => {
 
